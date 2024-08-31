002387a94d2059c82eb8b182c373f94893b19533
Roman Kurakin <rkurakin@moysklad.ru>
2024-08-31 11:52:04 +0300

    h3 подтягиваем еще конфигов, делаем main.js тайпскриптовым.

--- webpack.development.js
+++ webpack.development.js
@@ -3,4 +3,26 @@ const config = require('./webpack.config');
 
 module.exports = merge(config, {
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ 'style-loader', 'css-loader', 'postcss-loader'],
+                exclude: /\.module\.css$/i,
+            },
+            {
+                test: /\.module\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    },
+                    'postcss-loader'
+                ],
+            },
+        ]
+    }
 });
