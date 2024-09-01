e46f41b2b4eb60796ab0829ade021b56150b02e1
Roman Kurakin <rkurakin@moysklad.ru>
2024-09-01 21:56:00 +0300

    h3: натягиваем старую версию на реакт, получаем тест зеленым.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './app.css'
+import {Header} from '../header/header'
 import {Footer} from '../footer/footer'
 
 export const App: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <>
-            <header></header>
+            <header><Header onClick={() => {setShowModal(true);}}/></header>
             <main></main>
             <footer><Footer/></footer>
         </>
