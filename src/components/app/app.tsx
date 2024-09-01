3b36f26e12d9db6178bd11f5a56bb87fda3a28c0
Roman Kurakin <rkurakin@moysklad.ru>
2024-09-01 03:24:32 +0300

    h3: Добавляем сам компонент App, делаем тест зеленым.

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './app.css'
+
+export const App: React.FC = () => {
+    return (
+        <>
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import 'flowbite';
 import './style.css'
 
-import './app/app'
+import {createRoot} from "react-dom/client";
+import {App} from "./components/app/app";
+import {StrictMode} from "react";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+);
