49b90f420116fd8121dd7b894e96b9357ca393c6
Roman Kurakin <rkurakin@moysklad.ru>
2024-09-07 18:59:20 +0300

    h4: отключаем localStorage.

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,11 +1,8 @@
 import React, {useEffect} from "react";
-import { v4 as uuidv4 } from 'uuid';
 
-import {useLocalStorage} from "../../lib/local_storage"
 import {TableTemplate} from "./table_template";
 import {CounterPartyRecord} from "../../model/counter_party_record.type";
 import './table.css';
-import itemsInitial from './items';
 import {useDataContext} from "../../hooks/useDataContext/datacontext.hook";
 
 
@@ -16,28 +13,7 @@ type iProps = {
 
 
 export const Table: React.FC<iProps> = ({data, modalCallBack}) => {
-    const [items, setItems] = useLocalStorage('items', itemsInitial);
     const dataContext = useDataContext();
-
-    const dataContext2 = {
-        loadAll: () => {
-            console.log("loading...");
-        },
-        add: (item: CounterPartyRecord) => {
-            item.id = uuidv4();
-            setItems([...items, item]);
-        },
-        apply: (item: CounterPartyRecord) => {
-            const newItems = items.map((v: CounterPartyRecord) => {
-                return v.id == item.id ? item : v;
-            });
-            setItems(newItems)
-        },
-        delete: (id: string) => {
-            setItems(items.filter(((item: CounterPartyRecord) => item.id != id)));
-        },
-        items: items
-    }
 
     useEffect(() => {
         dataContext.loadAll();
